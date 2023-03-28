6c919f08db6b8d5d38c13626f7d5ef0e9bef79df
benkaan001 <benkaan001@gmail.com>
2023-03-27 23:59:25 -0500

    add legacy files

---
+++ legacy/pay/order.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass, field
+from enum import Enum
+
+class OrderStatus(Enum):
+    OPEN = 'open'
+    PAID = 'paid'
+
+@dataclass
+class Lineitem:
+    name: str
+    price: str
+    quantity: int = 1
+
+    @property
+    def total(self) -> int:
+        """ Returns the total cost of the line item (price * quantity)."""
+        return self.price * self.quantity
+
+@dataclass
+class Order:
+    line_items: list[Lineitem] = field(default_factory=list)
+    status: OrderStatus = OrderStatus.OPEN
+
+    @property
+    def total(self) -> int:
+        """ Returns the total cost of the order (sum of all line items)."""
+        return sum(item.total for item in self.line_items)
+
+    def pay(self) -> None:
+        """ Changes the status of the order to PAID."""
+        self.status = OrderStatus.PAID
