63a4d9081b8dea9f5dba4a1161aed378615cfde2
benkaan001 <benkaan001@gmail.com>
2023-05-04 22:35:06 -0500

    add doc strings

--- legacy_test_added/pay/tests/test_line_item.py
+++ legacy_test_added/pay/tests/test_line_item.py
@@ -1,9 +1,13 @@
 from pay.order import LineItem
 
 def test_line_item_total() -> None:
+    """Test that the total price of a LineItem is calculated correctly."""
     line_item = LineItem(name='Test', price=1000)
     assert line_item.total == 1000
 
 def test_line_item_quantity() -> None:
+    """Test that the total price of a LineItem is calculated
+    correctly when the quantity is greater than 1."""
     line_item = LineItem(name='Test', price=1000, quantity=5)
     assert line_item.total == 5000
+
--- legacy_test_added/pay/tests/test_order.py
+++ legacy_test_added/pay/tests/test_order.py
@@ -1,21 +1,25 @@
 from pay.order import Order, LineItem, OrderStatus
 
 def test_empty_order_total() -> None:
+    """Test that the total of an empty order is 0."""
     order = Order()
     assert order.total == 0
 
 def test_order_total() -> None:
+    """Test that the total of an order with a single line item is calculated correctly."""
     order = Order()
     order.line_items.append(LineItem(name="Coke", price=100))
     assert order.total == 100
 
 def test_order_total_with_multiple_items() -> None:
+    """Test that the total of an order with multiple line items is calculated correctly."""
     order = Order()
     order.line_items.append(LineItem(name="Coke", price=100))
     order.line_items.append(LineItem(name="Pepsi", price=100))
     assert order.total == 200
 
 def test_order_pay() -> None:
+    """Test that an order can be paid and its status is updated accordingly."""
     order = Order()
     order.pay()
     assert order.status == OrderStatus.PAID
