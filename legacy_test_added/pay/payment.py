c834dd514afc71e02cf313210c4fcba9775dc027
benkaan001 <benkaan001@gmail.com>
2023-05-04 23:00:27 -0500

    hide API key

--- legacy_test_added/pay/payment.py
+++ legacy_test_added/pay/payment.py
@@ -1,5 +1,13 @@
 from pay.order import Order
 from pay.processor import PaymentProcessor
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+# Add environment variable support
+API_KEY = os.getenv('API_KEY')
 
 def pay_order(order: Order):
     if order.total == 0:
@@ -7,6 +15,6 @@ def pay_order(order: Order):
     card = input('Please enter your card number: ')
     month = int(input('Please enter the card expiration month: '))
     year = int(input('Please enter the card expiration year: '))
-    payment_processor = PaymentProcessor('0super-1secret-2api-3key')
+    payment_processor = PaymentProcessor(API_KEY)
     payment_processor.charge(card, month, year, amount=order.total)
     order.pay()
