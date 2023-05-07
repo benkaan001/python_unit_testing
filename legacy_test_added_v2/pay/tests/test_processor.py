4aa660612098e4974a3bb840a973ac75a96f77bb
benkaan001 <benkaan001@gmail.com>
2023-05-06 23:09:31 -0500

    add doc string and missing type hints

--- legacy_test_added_v2/pay/tests/test_processor.py
+++ legacy_test_added_v2/pay/tests/test_processor.py
@@ -43,28 +43,28 @@ def test_card_has_invalid_date() -> None:
     with pytest.raises(ValueError):
         processor.charge(CARD_NUMBER, 12, 1900, 500)
 
-def test_card_number_valid_luhn():
+def test_card_number_valid_luhn() -> None:
     """
     Test that the Luhn checksum method returns True when provided with a valid card number.
     """
     processor = PaymentProcessor(API_KEY)
     assert processor.luhn_checksum(CARD_NUMBER)
 
-def test_card_number_invalid_luhn():
+def test_card_number_invalid_luhn() -> None:
     """
     Test that an error is raised when a charge is attempted with a card that has an invalid expiration date.
     """
     processor = PaymentProcessor(API_KEY)
     assert not processor.luhn_checksum("1234")
 
-def test_charge_card_valid():
+def test_charge_card_valid() -> None:
     """
     Test that a charge can be made with a valid card.
     """
     processor = PaymentProcessor(API_KEY)
     processor.charge(CARD_NUMBER, 12, 2024, 500)
 
-def test_charge_card_invalid():
+def test_charge_card_invalid() -> None:
     """
     Test that an error is raised when a charge is attempted with an invalid card.
     """
